e53430edfee97b1217e536caead01340666e5cb5
grudgemonkey <grudgemonkey65@gmail.com>
2022-01-31 11:16:31 -0500

    added alacritty config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes '(tsdh-dark))
  '(package-selected-packages '(org-roam)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -17,3 +20,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; set org agenda directory
+(setq org-agenda-files '("~/OrgMode/OrgAgenda/"))
+
+;; opens todo file every time emacs opens
+(find-file "~/OrgMode/OrgAgenda/todo.org")
