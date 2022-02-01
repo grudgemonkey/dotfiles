51284c7aaa851254c7a0a9b64e1f04bf262a6b18
grudgemonkey <grudgemonkey65@gmail.com>
2022-01-31 19:30:20 -0500

    made lemonbar show linux version on the right

--- lemonbar.sh
+++ lemonbar.sh
@@ -7,9 +7,16 @@ Clock() {
         echo -n "$DATETIME"
 }
 
+# Define function that prints linux version to the screen
+Kernel() {
+         LINUX=$(uname -r)
+
+     
+         echo -n "$LINUX"
+}
 # Print the clock
 
 while true; do
-        echo "%{c}%{F#FFFF00}%{B#0000FF} $(Clock) %{F-}%{B-}"
+	echo "%{c}%{F#FFFF00}%{B#0000FF} $(Clock) %{F-}%{B-}  %{r} $(Kernel)"
         sleep 1
 done
