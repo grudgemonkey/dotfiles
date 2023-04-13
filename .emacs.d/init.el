51fad6dc5d45589362217ffc751e9badde3a49ab
grudgemonkey <grudgemonkey65@gmail.com>
2023-04-13 18:36:14 -0400

    fixed emacs init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,13 +15,13 @@
 ;; package shit
 (require 'package)
 
-(setq package-archives '(("melpa"  , "https://melpa.org/packages/")
-			 ("org"  ,  "https://orgmode.org/elpa/")
-			 ("elpa"  ,  "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org"  .  "https://orgmode.org/elpa/")
+			 ("elpa"  .  "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
-(unless  package-archive-contents
-   (package-refresh-contents))
+(unless package-archive-contents
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
