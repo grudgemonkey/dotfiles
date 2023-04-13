f2b2d87b79ea452ac2df2f5a683e41fa8b96088f
grudgemonkey <grudgemonkey65@gmail.com>
2023-04-13 19:09:26 -0400

    emacs shenanigans

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,13 +3,16 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
-(set-fringe-mode 10)
+(set-fringe-mode 4)
 
 (menu-bar-mode -1) 
 
 (setq visible-bell t)
 
-(load-theme 'tango-dark)
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 
 ;; package shit
@@ -28,3 +31,27 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 20)))
