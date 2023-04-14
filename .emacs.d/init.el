060818b9c4d730b2188816c346d1531d084f79ac
grudgemonkey <grudgemonkey65@gmail.com>
2023-04-13 22:24:50 -0400

    emac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -131,6 +131,12 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 ;;-------bullshit------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -138,7 +144,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil general all-the-icons doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel)))
+   '(evil-collection evil-selection evil general all-the-icons doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
